754709eff123ce82f92012ca831748adbdf9d9e1
Donderstal <daanonderstal@hotmail.com>
2020-03-11 14:26:09 +0100

    new structure for project

---
+++ scraper-cli.py
@@ -0,0 +1,6 @@
+# This file should capture the CLI arguments
+# ( I might possibly redo this in a graphical interface at some point )
+
+## Catch CLI arguments
+## Discern what they mean
+## Start scraper with correct params
---
+++ scraper/scrapers/__init__.py
@@ -0,0 +1,7 @@
+from scrapers.scrapeInterveste import scrape 
+
+# Write 'master function' here
+# Which will call all scrape functions in folder
+
+def scrapeAll() : 
+    scrape()
